88783ee79a76c58b71a958989bf5c0adf891c72d
James Bielman <jamesjb@gmail.com>
2023-03-08 07:48:03 -0800

    Add some Windows configuration stuff.

---
+++ setup.ps1
@@ -0,0 +1,10 @@
+﻿#
+# setup.ps1 --- Set up symlinks for a new Windows system.
+#
+# Copyright (C) 2023, James Bielman <jamesjb@gmail.com>
+# All Rights Reserved.
+#
+
+New-Item -ItemType SymbolicLink -Path "$([Environment]::GetFolderPath("LocalApplicationData"))/nvim" -Target "$(Get-Location)/nvim"
+New-Item -ItemType Directory -Path "$HOME/.config/wezterm" -Force
+New-Item -ItemType SymbolicLink -Path "$HOME/.config/wezterm/wezterm.lua" -Target "$(Get-Location)/wezterm/wezterm.lua"
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,6 +5,8 @@
 -- All Rights Reserved.
 --
 
+local wezterm = require("wezterm")
+
 -- Default configuration for all OSes.
 local config = {
     font_size = 14,
@@ -12,6 +14,11 @@ local config = {
     harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
     unix_domains = {},
 }
+
+-- Use Powershell by default on Windows.
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+    config.default_prog = { "powershell" }
+end
 
 -- Check for machine-specific Wezterm configuration. If the
 -- "wezterm_local.lua" file exists, it must return a function
